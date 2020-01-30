c4dab76cdc393f3710669b5aec98cc8942d6bf15
Justin Dennison <justin1dennison@gmail.com>
2020-01-30 09:53:55 -0500

    updated build

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,10 +3,13 @@ targets=${@-"darwin/amd64 linux/amd64  windows/amd64"}
 for target in $targets; do
   os="$(echo $target | cut -d '/' -f1)"
   arch="$(echo $target | cut -d '/' -f2)"
-  output="${os}_${arch}_goCat"
+  directory="${os}_${arch}"
+  mkdir -p $directory
+  output="${directory}/goCat"
 
   echo "----> Building project for: $target"
   GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $output
+  zip -j "${directory}.zip" $directory
 done
 
 
