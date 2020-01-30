917b029376c94ec2d4cd683fde0ff4cb67dab4cc
Justin Dennison <justin1dennison@gmail.com>
2020-01-30 09:04:06 -0500

    fixing packaging

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,14 +3,13 @@ targets=${@-"darwin/amd64 linux/amd64  windows/amd64"}
 for target in $targets; do
   os="$(echo $target | cut -d '/' -f1)"
   arch="$(echo $target | cut -d '/' -f2)"
-  output="${release_path}/${repo_name}_${os}_${arch}"
+  output="${os}_${arch}_goCat"
 
   echo "----> Building project for: $target"
   GOOS=$os GOARCH=$arch CGO_ENABLED=0 go build -o $output
-  zip -j $output.zip $output > /dev/null
 done
 
 
-echo "-----> Build is complete. List of files at $release_path:"
+echo "-----> Build is complete. List of files:"
 cd $release_path
 ls -al
